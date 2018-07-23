d34c7142158a6dbc203b2f40add79fba84cb5cce
Beck <1008887@student.sae.edu.au>
2018-07-23 10:48:05 +1000

    Started making fakebook. played around with the AI to get it to work. Will need to redo that at home.

---
+++ Assets/Scripts/TenantSpawn.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TenantSpawn : MonoBehaviour {
+
+    public float timeLeft;
+    public float keySoundTime;
+
+    public Transform AISpawnPoint;
+    public GameObject AICharacter;
+
+    //private void OnEnable()
+    //{
+      //  EventManager.StartListening("OnNewLevel", TimeLeftChange);
+    //}
+
+    // Update is called once per frame
+    void Update () {
+
+        timeLeft -= Time.deltaTime;
+
+        if (timeLeft == keySoundTime)
+        {
+            //Play Audio clip of the door rattling.
+        }
+
+        if (timeLeft == 0)
+        {
+            Instantiate(AICharacter, AISpawnPoint);
+        }
+	}
+
+    void TimeLeftChange()
+    {
+        timeLeft -= 3;
+    }
+}
