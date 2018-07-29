86b00d0ccf3f9537c819ec41a172d0fcdaea5995
Unknown <1008887@student.sae.edu.au>
2018-07-29 21:18:27 +1000

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/EdwardComputer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EdwardComputer : MonoBehaviour {
+
+    Computer regularComputerScript;
+    Inventory inv;
+
+    public GameObject fakebookPanel;
+    bool panelActive = false;
+
+    private void OnEnable()
+    {
+        EventManager.StartListening("OnEdwardComputer", Fakebook);
+        EventManager.StartListening("OnFinalLevel", ScriptSwitch);
+    }
+
+    private void OnDisable()
+    {
+        EventManager.StopListening("OnEdwardComputer", Fakebook);
+        EventManager.StopListening("OnFinalLevel", ScriptSwitch);
+    }
+   
+    // Use this for initialization
+    void Start () {
+        regularComputerScript = GetComponent<Computer>();
+        inv = GetComponent<Inventory>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Fakebook ()
+    {
+        panelActive = true;
+        fakebookPanel.SetActive(true);
+    }
+
+    public void ScriptSwitch ()
+    {
+        regularComputerScript.enabled = true;
+        inv.enabled = true;
+        this.enabled = false;
+    }
+}
