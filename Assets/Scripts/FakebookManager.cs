80cc8636a82976dc6c824038ad6a72fa8d7ea663
Beck <1008887@student.sae.edu.au>
2018-07-30 13:05:51 +1000

    tried to get some stuff with fakebook working

---
+++ Assets/Scripts/FakebookManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FakebookManager : MonoBehaviour {
+
+    public GameObject[] profiles;
+    public string buttonName;
+
+	// Use this for initialization
+	void Start () {
+        buttonName = gameObject.name;
+	}
+
+    //takes all of the profiles avaliable in the array
+    //checks which one has the same name as the button
+    //enables it
+    void OpenProfile ()
+    {
+        foreach (GameObject profile in profiles)
+        {
+            if (profile.name.EndsWith(buttonName))
+            {
+                profile.gameObject.SetActive(true);
+                return;
+            }
+        }
+    }
+}
