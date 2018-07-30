867dd3bcbd0f2c5d265831ed283a3fb52d8c3a5f
Unknown <1008885@student.sae.edu.au>
2018-07-30 13:05:52 +1000

    cat follows player

---
+++ Assets/Scripts/CatFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatFollow : MonoBehaviour {
+
+    public Transform target; 
+    public Vector3 catPosition;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        //CatFollowing();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        catPosition = transform.position;
+        target = GameObject.FindWithTag("Catnip").transform;
+        transform.position = Vector3.MoveTowards(catPosition, target.transform.position, speed);
+    }
+
+    public void CatFollowing ()
+    {
+       
+    }
+}
