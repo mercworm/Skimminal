66d649c12f51e65953f4f92fb7b33055ef87de7d
Beck <1008887@student.sae.edu.au>
2018-07-30 10:20:52 +1000

    added a name canvas for every tenant so the player gets a better idea of what's going on

---
+++ Assets/Scripts/TenantNameCanvas.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TenantNameCanvas : MonoBehaviour {
+
+    private void OnEnable()
+    {
+        EventManager.StartListening("OnHideTenantName", Hide);
+    }
+
+    public void Hide ()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
