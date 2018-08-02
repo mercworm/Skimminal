00eaaa220b52564eab12d1467b02601727342ecf
Beck <1008887@student.sae.edu.au>
2018-08-02 13:26:48 +1000

    Relationship Manager version2 added.

---
+++ Assets/Toolin/RelationshipManager/RelationshipController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RelationshipController : MonoBehaviour
+{
+    public RelationshipManager relMan;
+	void Start ()
+    {
+        relMan.Initialise();
+        relMan.EnforceStartingState();
+	}
+}
--- Assets/Toolin/RelationshipManager/RelationshipManager.cs
+++ Assets/Toolin/RelationshipManager/RelationshipManager.cs
@@ -2,17 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RelationshipManager: MonoBehaviour
+[System.Serializable]
+public class RelationShipEntry
 {
-    public SORelationships[] Relationship;
+    public string Person1;
+    public string Person2;
+    public int Status;
+}
+
+[CreateAssetMenu]
+public class RelationshipManager: ScriptableObject
+{
+    [Tooltip("List all characters name in game")]
+    public string[] characterNames;
+
+    private void Start()
+    {
+        Initialise();
+    }
+
+    public void Initialise()
+    {
+        for (int i = 0; i < characterNames.Length; i++)
+        {
+            for (int j = i+1; j < characterNames.Length; j++)
+            {
+                Relationships.Add(new RelationShipEntry() { Person1 = characterNames[i], Person2 = characterNames[j], Status = 0 });
+            }
+        }
+
+        EnforceStartingState();
+    }
+
+    [Tooltip("If you want people to start with a different relationships status than default(0)")]
+    public RelationShipEntry[] startingRelationShipState;
+
+    //init with default relationships
+    public void EnforceStartingState()
+    {
+        foreach (var item in startingRelationShipState)
+        {
+            UpdateRelationship(item.Person1, item.Person2, item.Status);
+        }
+    }
+
+
+    private List<RelationShipEntry> Relationships = new List<RelationShipEntry>();
 
     public void UpdateRelationship(string person1, string person2, int Status)
     {
-        for (int i = 0; i < Relationship.Length; i++)
+        for (int i = 0; i < Relationships.Count; i++)
         {
-            if (Relationship[i].Person1.Contains(person1) && Relationship[i].Person2.Contains(person2) || Relationship[i].Person1.Contains(person2) && Relationship[i].Person2.Contains(person1))
+            if (Relationships[i].Person1.Contains(person1) && Relationships[i].Person2.Contains(person2) || Relationships[i].Person1.Contains(person2) && Relationships[i].Person2.Contains(person1))
             {
-                Relationship[i].Status += Status;
+                Relationships[i].Status += Status;
             }
         }
         Debug.Log(person1 + person2 + Status);
@@ -20,11 +63,11 @@ public class RelationshipManager: MonoBehaviour
 
     public int CurrentRelationshipStatus(string person1, string person2)
     {
-        for (int i = 0; i < Relationship.Length; i++)
+        for (int i = 0; i < Relationships.Count; i++)
         {
-            if (Relationship[i].Person1.Contains(person1) && Relationship[i].Person2.Contains(person2) || Relationship[i].Person1.Contains(person2) && Relationship[i].Person2.Contains(person1))
+            if (Relationships[i].Person1.Contains(person1) && Relationships[i].Person2.Contains(person2) || Relationships[i].Person1.Contains(person2) && Relationships[i].Person2.Contains(person1))
             {
-                return Relationship[i].Status;
+                return Relationships[i].Status;
             }
         }
         return 0;
