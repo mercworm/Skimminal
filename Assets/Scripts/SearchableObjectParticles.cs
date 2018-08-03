ee4359ef359ea3a2123da2bcd4ced69b99269a30
Unknown <1008887@student.sae.edu.au>
2018-08-03 17:10:49 +1000

    Particle Effect Searchable Object
    A particle effect now plays every time the player is within the radius for each searchable object

---
+++ Assets/Scripts/SearchableObjectParticles.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SearchableObjectParticles : MonoBehaviour {
+
+    public ParticleSystem ps;
+
+    private void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            ps.Play();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            ps.Stop();
+        }
+    }
+}
