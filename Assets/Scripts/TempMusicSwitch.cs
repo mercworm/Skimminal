83512d841ff79cd82c4c0490d3a7bcbf93b5bcec
Beck <1008887@student.sae.edu.au>
2018-08-06 11:28:39 +1000

    updated so the music starts when the tenant comes home

---
+++ Assets/Scripts/TempMusicSwitch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempMusicSwitch : MonoBehaviour {
+
+    public AudioClip run;
+    AudioSource source;
+
+    private void OnEnable()
+    {
+        EventManager.StartListening("OnTenantSpawn", Music);
+    }
+
+    // Use this for initialization
+    void Start () {
+        source = GetComponent<AudioSource>();
+	}
+
+    public void Music ()
+    {
+        source.clip = run;
+        source.Play();
+    }
+}
